75fad0a77b595520b4272f699e9df9b830846282
Mighty Men <wpq163yx@163.com>
2018-03-21 15:17:45 +0800

    v1.0.0
    上传源码

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+﻿import {Animal,GztUtils,Circle} from "./utils"
+import $ from "./jquery"
+
+
+class Dog extends Animal{
+  constructor(){
+    super();
+    console.log("==constructor dog");
+  }
+}
+
+document.getElementById("demo").innerHTML=new Dog().sayHello();
+$("#demo_2").html(GztUtils.add(10,5));
+document.getElementById("demo_3").innerHTML=new Circle().sayCircleArea(1) ;
+
+
+
+
+
---
+++ src/js/utils.js
@@ -0,0 +1,40 @@
+﻿class Animal{
+  constructor(){
+    console.log("==constructor animal==") ;
+  }
+
+  sayHello(){  
+      return "hello,Animal";
+  }
+}
+ 
+let GztUtils={
+  add(x,y){
+     return x+y;
+  }
+}
+
+
+class Circle{
+  constructor(radius=1){
+		this.PI = 3.14;
+		this.nR = radius;
+		this.sMessage = "半径为"+this.nR+"的圆面积:";
+  }
+  getArea(){  
+    return this.PI * this.nR * this.nR;
+  }
+  sayCircleArea(){
+		return this.sMessage + this.getArea();
+  }
+}
+
+module.exports = {
+  Animal: Animal,
+  GztUtils : GztUtils,
+  Circle : Circle
+};
+
+
+
+
